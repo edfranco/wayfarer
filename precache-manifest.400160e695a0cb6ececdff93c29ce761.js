f3ebc1383e40f182e89a7759a75de48157b817ad
edfranco <edfranco415@gmail.com>
2019-09-18 19:54:40 -0700

    Updates

---
+++ precache-manifest.400160e695a0cb6ececdff93c29ce761.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86387aa1418f0be0ebcffe84a4306dd1",
+    "url": "/wayfarer/index.html"
+  },
+  {
+    "revision": "ef8c88c7c45fa57ae3cc",
+    "url": "/wayfarer/static/css/main.38fcec1d.chunk.css"
+  },
+  {
+    "revision": "820fc0a00cc44f8318a7",
+    "url": "/wayfarer/static/js/2.d43f7aeb.chunk.js"
+  },
+  {
+    "revision": "ef8c88c7c45fa57ae3cc",
+    "url": "/wayfarer/static/js/main.25bd1e61.chunk.js"
+  },
+  {
+    "revision": "8db35a9285176c44401d",
+    "url": "/wayfarer/static/js/runtime~main.9aa6ee55.js"
+  }
+]);
